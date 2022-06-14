1a725b80e19d0a7949d8b84713f1c6af7c21c5ee
Spencer Bernard <105369089+sjbern@users.noreply.github.com>
2022-06-14 08:21:27 -0600

    Add files via upload
    updated file, previous would not work

--- plates.py
+++ plates.py
@@ -7,13 +7,15 @@ def main():
 
 def valid(s):
     if 2 <= len(s) <= 6 and s[0:2].isalpha() and s.isalnum():
-
         for char in s:
             if char.isdigit():
                 index = s.index(char)
-                if s[index].isdigit() and int(char) != 0:
-                    return True
+                if s[index:].isdigit():
+                        return True
+                elif s[index].isdigit() and int(char) != 0:
+                        return True
                 else:
-                    return False
+                        return False
         return True
 main()
+
